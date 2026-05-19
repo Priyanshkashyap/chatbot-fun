d4415ae8ce37ef6d5b794fa34d5a97a25f420ccb
Priyanshkashyap <kpriyansh2121@gmail.com>
2026-05-20 00:54:04 +0530

    added chat history nd memory

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,38 +1,46 @@
-import OpenAI from "openai"; // Even though you are using Groq here, Groq provides an OpenAI-compatible API, so this OpenAI SDK can still be used.
+import OpenAI from "openai";
 import { NextResponse } from "next/server";
 
 const client = new OpenAI({
   apiKey: process.env.GROQ_API_KEY,
+
   baseURL: "https://api.groq.com/openai/v1",
 });
-// means Use my Groq key and send requests to Groq’s API.
+
 export async function POST(req: Request) {
+
   try {
-    const body = await req.json();// This reads the incoming request body and converts it to JavaScript object form(which is not so different from normal form jsut refines it)
+
+    const body = await req.json();
 
     const completion =
       await client.chat.completions.create({
+
         model: "llama-3.3-70b-versatile",
-        // chooses which model to use and use the chat-completions endpoint and wait for the model to generate a reply.completion will store the full model response
+
         messages: [
+
+          // System prompt
           {
             role: "system",
-            content: "You are a helpful AI tutor.", // The system prompt SHOULD be sent on every request.LLMs do NOT remember anything between requests.Every API call is completely fresh.
+            content:
+              "You are a helpful AI tutor.",//  send everytime as llm forgets everything thus sees entire chat at once
           },
-          {
-            role: "user",
-            content: body.message,
-          },
+
+          // Entire conversation history
+          ...body.messages,
+
         ],
+
       });
 
     return NextResponse.json({
-      reply: // can be named anything
-        completion.choices[0].message.content, // extracts the model’s reply text
-        //the API returns a list of possible completions usually the first one is the answer you want thus [0]
+      reply:
+        completion.choices[0].message.content,
     });
 
   } catch (error: any) {
+
     console.log(error);
 
     return NextResponse.json(
@@ -41,5 +49,6 @@ export async function POST(req: Request) {
       },
       { status: 500 }
     );
+
   }
 }
